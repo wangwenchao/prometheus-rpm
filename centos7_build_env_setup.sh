50032260fb4926338b37f5e70f060c675680c352
wangwenchao <wang.wenchao.cn@gmail.com>
2018-12-29 18:30:38 +0800

    update scripts

--- centos7_build_env_setup.sh
+++ centos7_build_env_setup.sh
@@ -1,10 +1,10 @@
-sudo yum install epel-release
+sudo yum install -y epel-release
 
-sudo yum install https://centos7.iuscommunity.org/ius-release.rpm
+sudo yum install -y https://centos7.iuscommunity.org/ius-release.rpm
 
-sudo yum install python36u
-sudo ln -s /bin/python3.6 /bin/python3
-sudo yum install python36u-pip
-sudo ln -s /bin/pip3.6 /bin/pip3
+sudo yum install -y python36u
+sudo ln -s /usr/bin/python3.6 /bin/python3
+sudo yum install -y python36u-pip
+sudo ln -s /usr/bin/pip3.6 /bin/pip3
 
 pip3 install -r requirements.txt
